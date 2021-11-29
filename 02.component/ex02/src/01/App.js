507d1b7be8571d402f7177b463d62a33401f131e
sysyo <youngiya77@gmail.com>
2021-11-29 11:22:59 +0900

    styling #1

---
+++ 02.component/ex02/src/01/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function () {
+    const h1Style = {
+        width: 200,
+        height: 50,
+        padding: 5,
+        color: '#111',
+        backgroundColor: '#eeff99'
+    }
+
+    return (
+        <div id='App'>
+            <h1 style={ h1Style }>Inline Styling</h1>
+        </div>
+    )
+
+}
---
+++ 02.component/ex02/src/02/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './assets/css/App.css';
+
+export default function () {
+
+    return (
+        <div id='App'>
+            <h1>{`Normal CSS (css-loader options: { modules: false })`}</h1>
+        </div>
+    )
+
+}
+
+
