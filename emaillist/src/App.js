380fc5b229c57d6d543deb84ce0b6140ac3ec901
sysyo <youngiya77@gmail.com>
2021-12-02 12:23:28 +0900

    emaillist #3

--- emaillist/src/App.js
+++ emaillist/src/App.js
@@ -1,16 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import RegisterForm from './RegisterForm';
-import './assets/scss/App.scss';
 import SearchBar from './SearchBar';
 import Emaillist from './Emaillist';
 
+import './assets/scss/App.scss';
+import data from './assets/json/data.json';
 
 export default function () {
-    return (
+    const [keyword, setKeyword] = useState('');
+
+    const notifyKeywordChanged = (keyword) => {
+        console.log(keyword); // 자식이 부모 호출
+        setKeyword(keyword); // 다시 상태값 변화
+    }
+        return (
         <div className={'App'}>
             <RegisterForm />
-            <SearchBar />
-            <Emaillist />
+            <SearchBar callback = { notifyKeywordChanged } /> 
+            <Emaillist keyword={ keyword }  emails = { data } />
         </div>
     )
 }
+
+// callback - 자식이 부모에게 전달하는 용도로 사용하는 프로퍼티 (임시지정이름)
--- emaillist/src/SearchBar.js
+++ emaillist/src/SearchBar.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './assets/scss/SearchBar.scss';
 
-const SearchBar = () => {
+const SearchBar = ({callback}) => {
+    const [keyword, setKeyword] = useState('');
+    const onInputChanged = (e) => {
+        setKeyword(e.target.value); 
+        callback(e.target.value)
+    };
+
     return (
         <div>
-            <div class = {styles.Searchbar} >
-                찾기: <input type='text' placeholder='찾기' />
+            <div className = {styles.Searchbar} >
+                찾기: <input type='text' placeholder='찾기' value={ keyword } onChange = { onInputChanged } />
             </div>
         </div>
     );
 };
 
 export default SearchBar;
+
+// 1. value={ keyword }: 제어컴포넌트 - keyword : 상태
+// 2. const [keyword, setKeyword] = useState(''); / import 명시 - import React, { useState } from 'react'; 
+// 3. onChange = { (e) => setKeyword(e.target.value) } // 이벤트로 받기
