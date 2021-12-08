db2a663775a1bdad7042408c463b596c3e606833
sysyo <youngiya77@gmail.com>
2021-12-08 20:24:55 +0900

    router2

--- 03.routing/src/02/App.js
+++ 03.routing/src/02/App.js
@@ -6,31 +6,45 @@ import Guestbook from "./component/Guestbook";
 export default function App() {
     const [route, setRoute] = useState({page: '/'});
 
+    useEffect(() => {
+        const handlePopState = () => setRoute(e.state);
 
+        window.addEventListener('popstate', handlePopState);
+        return () => {
+            window.removeEventListener('popstate', handlePopState);
+        };
+    }, []);
 
     const handleLinkClick = (e) => {
         e.preventDefault();
+
+        const url = e.target.href.substr(e.target.href.lastIndexOf('/'));
+        // 뒤로가기 지원
+        window.history.pushState({page: url}, e.target.text, url);
+        setRoute({page: url});
     }
 
-    const router = function () {
-        let component = null;
-        switch (route.page) {
-            case '/':
-                component = <Main/>;
-                break;
-            case '/gallery':
-                component = <Gallery/>;
-                break;
-            case '/guestbook':
-                component = <Guestbook/>;
-                break;
-        }
-
-        return component;
-    };
+    
 
     return (
         <div>
+            {
+                 (() => {
+                    switch(route.page){
+                        case '/' :
+                            return <Main />;
+                        case '/guestbook' :
+                            return <Guestbook />;
+                        case '/gallery' :
+                            return <Gallery />;
+                        default :
+                            return null;
+                    }
+            
+                    return <div>{`${route} 에 맞는 컴포넌트를 리턴 합니다.`}</div>
+                })()
+            }
+            {/* <div>{route.page}</div> */}
             <ul>
                 <li><a href={'/'} onClick={handleLinkClick}>[Main]</a></li>
                 <li><a href={'/gallery'} onClick={handleLinkClick}>[Gallery]</a></li>
